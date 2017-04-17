1669b2516f4526ec0aedd01fce833c85bd3f0bca
Ben Foxall <benfoxall@gmail.com>
2017-04-17 22:58:06 +0100

    Move cjs to top level
    No more rollup, because I figure that’ll be something done at the other
    side of it.

--- _build.sh
+++ _build.sh
@@ -2,11 +2,6 @@
 # Copyright (c) 2013 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission.
 cd `dirname $0`
 
-mkdir -p es
-mkdir -p cjs
-
-rm es/* cjs/*
-
 DOCS=EspruinoDocs
 MODULES=`find $DOCS/devices $DOCS/modules $DOCS/boards -name "*.js"`
 
